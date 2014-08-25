5cf67bd93d2071af82e3b797eb02e7eae94b59fb
Foulgit <yumolun@hotmail.com>
2014-08-26 01:44:44 +0200

    update 0826

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="keywords" content="yumolun,molen,molun yu,余默伦,resume"/>
   <title>Molen's Resume</title>
   <link rel="shortcut icon" href="favicon.ico">
+  <link href="css/onepage-scroll.css?version=<?=md5_file('css/onepage-scroll.css')?>" rel="stylesheet" type="text/css" />
   <link href="css/index.css?version=<?=md5_file('css/index.css')?>" rel="stylesheet" type="text/css" />
   <!-- html5.js for IE less than 9 -->
   <!--[if lt IE 9]>
@@ -19,21 +20,39 @@
   <![endif]-->
 </head>
 <body>
-  <div id="container">
-    <header></header>
-    <div id="content">  
-      <div id="logo">M</div>
-      <div id="welcome">Welcome to Molen's</div>
-      <div class="main-nav">
-        <ul class="nav-bar">
-          <li><a href="http://blog.yumolun.com" target="_blank">Blog</a></li>
-          <li><a href="">Resume</a></li>
-          <li><a href="">Contact</a></li>
-        </ul>
+  <div class="main">
+    <section class="page1">
+      <nav class="main-nav">
+        <a class="first-link" href="http://www.yumolun.com">Molen's</a>
+        <a class="link" onclick="moveTo('.main', 5);" >Contact</a>
+        <a class="link" href="http://blog.yumolun.com">Blog</a>
+      </nav>
+      <div id="avatar">
+        <a href="http://www.yumolun.com">
+          <img src="images/avatar_500.png" alt="Molen">
+        </a>
       </div>
-    </div>-->
-  </div>
+      <h1>Molun Yu</h1>
+    </section>
+    <section class="page2">
+      <h2>Education</h2>
+      <hr>
+    </section>
+    <section class="page3">
+      <h2>Professional experience</h2>
+      <hr>
+    </section>
+    <section class="page4">
+      <h2>Skills</h2>
+      <hr>
+    </section>
+    <section class="page5">
+      <h2>Contact</h2>
+      <hr>
+    </section>
+  </div>   
   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="js/onepagescroll.js?version=<?=md5_file('js/onepagescroll.js')?>"></script>
   <script type="text/javascript" src="js/index.js?version=<?=md5_file('js/index.js')?>"></script>
 </body>
 </html>
