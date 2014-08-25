9b1f25978d28e0d84f25e4990c6374e47eba13a3
Foulgit <yumolun@hotmail.com>
2014-08-26 00:20:12 +0200

    initial

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="This is yumolun's website.余默伦的个人简历"/>
+  <meta name="keywords" content="yumolun,molen,molun yu,余默伦,resume"/>
+  <title>Molen's Resume</title>
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="css/index.css?version=<?=md5_file('css/index.css')?>" rel="stylesheet" type="text/css" />
+  <!-- html5.js for IE less than 9 -->
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!-- css3-mediaqueries.js for IE less than 9 -->
+  <!--[if lt IE 9]>
+    <script src="http://css3-mediaqueries-js.googlecode.com/svn/trunk/css3-mediaqueries.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <div id="container">
+    <header></header>
+    <div id="content">  
+      <div id="logo">M</div>
+      <div id="welcome">Welcome to Molen's</div>
+      <div class="main-nav">
+        <ul class="nav-bar">
+          <li><a href="http://blog.yumolun.com" target="_blank">Blog</a></li>
+          <li><a href="">Resume</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </div>
+    </div>-->
+  </div>
+  <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="js/index.js?version=<?=md5_file('js/index.js')?>"></script>
+</body>
+</html>
