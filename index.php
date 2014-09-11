a1438c28d6dfb64f77f62369fb24d536bca26a9d
Foulgit <yumolun@hotmail.com>
2014-09-11 18:50:16 +0200

    update 0911

--- index.php
+++ index.php
@@ -20,13 +20,8 @@
   <![endif]-->
 </head>
 <body>
-  <div class="main">
+  <div class="main"> 
     <section class="page1">
-      <nav class="main-nav">
-        <a class="first-link" href="http://www.yumolun.com">Molen's</a>
-        <a class="link" onclick="moveTo('.main', 5);" >Contact</a>
-        <a class="link" href="http://blog.yumolun.com">Blog</a>
-      </nav>
       <div id="avatar">
         <a href="http://www.yumolun.com">
           <img src="images/avatar_500.png" alt="Molen">
@@ -37,20 +32,67 @@
     <section class="page2">
       <h2>Education</h2>
       <hr>
+      <div class="content">
+        <table>
+          <tr>
+            <td class="left-col">2014</td>
+            <td class="right-col">Master's degree in Engineering obtained in the department of Telecommunications at National Institute of Applied Sciences of Lyon in France (INSA de Lyon)</td>    
+          </tr>
+          <tr>
+            <td class="left-col">2012</td>
+            <td class="right-col">Exchange student in the department of Communications and Networking at Aalto University in Helsinki, Finland</td>    
+          </tr>
+          <tr>
+            <td class="left-col">2011</td>
+            <td class="right-col">DUT (two years diploma) obtained in Networking and Telecommunications at IUT (University Institute of Technology) of University of Auvergne in France</td>    
+          </tr>
+        </table>
+      </div>
     </section>
     <section class="page3">
       <h2>Professional experience</h2>
       <hr>
+      <div class="content">
+        <table>
+          <tr>
+            <td class="left-col">2013<br><span style="font-size:20px">Six months</span></td>
+            <td class="right-col">Internship in CGI France, as an analyst developer
+              <ul>
+                <li>Worked in the CRM service to design solutions based on the clients’ requirements</li>
+                <li>Developed, tested and maintained softwares by using Pivotal, Oracle SQL Developer, Microsoft SQL Server, C#, VB Script, PHP and JavaScript<br><br><br></li>
+              </ul>
+            </td>    
+          </tr>
+          <tr>
+            <td class="left-col">2011<br><span style="font-size:20px">Two months</span></td>
+            <td class="right-col">Internship at ENSCCF in France, as a programmer
+                <ul>
+                <li>Migrated the school website from SPIP 1.9 to SPIP 2.0</li>
+                <li>Updated website plugins by using HTML, CSS, PHP, JavaScript and MySQL</li>
+              </ul>
+            </td>    
+          </tr>
+        </table>
+      </div>
     </section>
     <section class="page4">
-      <h2>Skills</h2>
+      <h2>Projects</h2>
       <hr>
     </section>
     <section class="page5">
+      <h2>Skills</h2>
+      <hr>
+    </section>
+    <section class="page6">
       <h2>Contact</h2>
       <hr>
     </section>
   </div>   
+  <nav class="main-nav">
+      <a class="first-link" href="http://www.yumolun.com">Molen's</a>
+      <a class="link" onclick="moveTo('.main', 5);" >Contact</a>
+      <a class="link" href="http://blog.yumolun.com">Blog</a>
+  </nav>
   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
   <script type="text/javascript" src="js/onepagescroll.js?version=<?=md5_file('js/onepagescroll.js')?>"></script>
   <script type="text/javascript" src="js/index.js?version=<?=md5_file('js/index.js')?>"></script>
