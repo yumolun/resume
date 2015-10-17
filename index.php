85a9b18d51ce65da0adc874771bedf7092cb6743
Molun YU <yumolun@hotmail.com>
2015-10-17 18:27:48 +0200

    update content

--- index.php
+++ index.php
@@ -179,7 +179,6 @@
   <nav class="main-nav">
       <a class="first-link" href="http://www.yumolun.com">Molen's</a>
       <a class="link" onclick="moveTo('.main', 6);" >Contact</a>
-      <a class="link" href="http://blog.yumolun.com" target="_blank">Blog</a>
   </nav>
   <script src="//code.jquery.com/jquery-1.11.0.min.js"></script>
   <script type="text/javascript" src="js/onepagescroll.js?version=<?=md5_file('js/onepagescroll.js')?>"></script>
